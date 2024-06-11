61ddf028dd421c58a9a0c5f3881abeb44065826c
kidanekal-k <kalupgk@gmail.com>
2024-06-11 22:32:13 +0300

    simple layout fixed

--- src/app/products/aa-policy-gen/page.tsx
+++ src/app/products/aa-policy-gen/page.tsx
@@ -1,13 +1,13 @@
 export default function AaPolicyGen() {
   return (
     <section className="pt-[3px]">
-      <div className="min-h-screen flex flex-col justify-center items-center">
+      {/* <div className="min-h-screen flex flex-col justify-center items-center">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 dark:text-white mb-4" data-aos="fade-up">AI-Assisted Policy Generation</h1>
         <p className="text-center text-gray-500 dark:text-gray-300 text-lg md:text-xl lg:text-2xl mb-8">AI automatically learning and updating security policies in real-time.</p>
         <div className="flex space-x-4">
           <a href="/request_demo" className="inline-flex items-center rounded bg-primary py-[10px] px-6 font-heading text-base text-white hover:bg-opacity-90 md:py-[14px] md:px-8">Demo On Demand</a>
         </div>
-      </div>
+      </div> */}
       {/* How it works section */}
       <section id="how-it-works" className="py-10 sm:py-32 relative overflow-hidden">
         {/* <div className="absolute inset-0 bg-gradient-to-r from-primary to-secondary"></div> */}
